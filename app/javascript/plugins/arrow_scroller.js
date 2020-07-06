e35549f8286197f3381bed6e673ac9dd5fdee3ba
Ulas <uli.will.code@protonmail.com>
2020-07-06 21:44:06 +0300

    basic html one can be done for today

---
+++ app/javascript/plugins/arrow_scroller.js
@@ -0,0 +1,13 @@
+const arrow = document.querySelector('.home-arrow')
+
+// const nav = document.querySelector('.nav-main')
+// const portfolioStart = document.querySelector('#projects')
+// const top = portfolioStart.getBoundingClientRect().top
+
+// arrow.addEventListener(
+//   'click', () => {
+//     let navHeight = nav.getBoundingClientRect().height
+//     window.scrollTo({top: top - navHeight,
+//       behavior: 'smooth'
+//     })
+//   })
