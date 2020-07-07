6a3a2a46f39dd491f79b1e4e0a0accdcfbeb694c
Ulas <uli.will.code@protonmail.com>
2020-07-07 14:58:10 +0300

    created views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact'
   get '/hood', to: 'pages#hood'
   get '/cocktail', to: 'pages#cocktail'
+  get '/exchange', to: 'pages#exchange'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
