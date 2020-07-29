573cc4d98421f50a0e63b17b0687634a3ac31029
Ulas <uli.will.code@protonmail.com>
2020-07-29 20:47:52 +0300

    started the final case study

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact'
   get '/hood', to: 'pages#hood'
   get '/cocktail', to: 'pages#cocktail'
-  get '/exchange', to: 'pages#exchange'
+  get '/herha_adoption', to: 'pages#herha_adoption'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
